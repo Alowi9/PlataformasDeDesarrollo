f16fb6117e3397c0cede3029b25be9ca7a89107b
maxip <maxip@DESKTOP-L6MO8I4>
2020-09-08 22:03:20 -0300

    part2

--- 08092020/ConsoleApp2/ConsoleApp2/Calculadora.cs
+++ 08092020/ConsoleApp2/ConsoleApp2/Calculadora.cs
@@ -7,6 +7,44 @@ namespace ConsoleApp2
     class Calculadora
     {
 
+        private int numero1;
+        public int Numero1
+        {
+            get
+            {
+                return numero1;
+            }
+            set
+            {
+                numero1 = value;
+            }
+
+        }
+
+
+        public int Numero2 { get; set; }
+        // otra forma de escribirlo 
+        // dentro del get o set se pueden cambiarla la visibilidad sea private, protected etc...
+
+
+        public void SetNumero1(int valor)
+        {
+            numero1 = valor;
+        }
+
+        public int GetNumero1()
+        {
+            return numero1;
+        }
+
+
+
+
+        private class Operacion
+        {
+
+        }
+
 
     }
 }
--- 08092020/ConsoleApp2/ConsoleApp2/Program.cs
+++ 08092020/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary1;
 
 namespace ConsoleApp2
 {
@@ -7,6 +8,17 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Calculadora calculadora = new Calculadora();
+            Class1 clase = new Class1();
+
+            calculadora.SetNumero1(13);
+            Console.WriteLine(calculadora.GetNumero1());
+
+            calculadora.Numero1 = 105;
+            Console.WriteLine(calculadora.Numero1);
+
+            calculadora.Numero2 = 20;
+            Console.WriteLine(calculadora.Numero2);
         }
     }
 }
