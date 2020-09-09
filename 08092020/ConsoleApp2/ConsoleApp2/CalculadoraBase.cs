9500996b8cfb5a2be6c9432bfe0e6334a193f87c
maxip <maxip@DESKTOP-L6MO8I4>
2020-09-08 22:51:01 -0300

    part5

---
+++ 08092020/ConsoleApp2/ConsoleApp2/CalculadoraBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public abstract class CalculadoraBase
+    {
+
+        private int indice { get; set; }
+
+        public virtual int Restar(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public abstract int Division(int valor1, int valor2);
+            
+    }
+}
