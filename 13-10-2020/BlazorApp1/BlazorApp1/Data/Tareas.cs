8b5beebfcb2e85d59b67b9629af6573d72354a90
maxip <maxip@DESKTOP-L6MO8I4>
2020-10-13 23:00:53 -0300

    implementando blazor

---
+++ 13-10-2020/BlazorApp1/BlazorApp1/Data/Tareas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorApp1.Data
+{
+    public class Tareas
+    {
+        [Key]
+        public int id { get; set; }
+        public String titulo { get; set; }
+        public String vencimiento { get; set; }
+        public String estimacion { get; set; }
+      /*  public int IdRecursos { get; set; }
+        public Recursos responsable { get; set; }*/
+        public bool estado { get; set; }
+
+
+        public Tareas() { }   //constructor implicito 
+
+        public Tareas(int id, string titulo, string vencimiento, string estimacion/* Recursos responsable*/, bool estado)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.vencimiento = vencimiento;
+            this.estimacion = estimacion;
+           /* this.responsable = responsable;*/
+            this.estado = estado;
+        }
+
+        //recordar incoorporar la clase Recursos más tarde!
+        
+
+
+    }
+}
---
+++ 13-10-2020/BlazorApp1/BlazorApp1/Data/TareasService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TareasService
+    {
+        public Tareas[] GetTareas()
+        {
+            Tareas[] resultado = new Tareas[5];
+
+            resultado[0] = new Tareas(1, "Tarea1", "30/06/2020", "10", false);
+            resultado[1] = new Tareas(2, "Tarea2", "30/07/2020", "11", false);
+            resultado[2] = new Tareas(3, "Tarea3", "30/08/2020", "12", false);
+            resultado[3] = new Tareas(4, "Tarea4", "30/09/2020", "13", false);
+            resultado[4] = new Tareas(5, "Tarea5", "30/10/2020", "14", false);
+
+
+            return resultado;
+        }
+    }
+}
